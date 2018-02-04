e68055d2fa0f0c7d89f42d8f3c74d6da78ff53c6
Nicolas San Jose <nicolasasanjose@gmail.com>
2018-02-03 17:15:38 -1000

    use assimp for mesh loading
    now using Assimp NuGet package for mesh loading

--- Ori-Engine/Systems/Graphics/Mesh.h
+++ Ori-Engine/Systems/Graphics/Mesh.h
@@ -2,14 +2,20 @@
 
 #include <d3d11.h>
 #include <DirectXMath.h>
-#include <memory>
-#include <unordered_map>
+
 #include <vector>
 #include <wrl.h>
 
 struct Vertex {
+	Vertex(DirectX::XMFLOAT3 a, DirectX::XMFLOAT2 b, DirectX::XMFLOAT3 c, DirectX::XMFLOAT3 d)
+	{
+		position = a;
+		tex_coord = b;
+		normal = c;
+		tangent = d;
+	}
 	DirectX::XMFLOAT3 position;
-	DirectX::XMFLOAT2 uv;
+	DirectX::XMFLOAT2 tex_coord;
 	DirectX::XMFLOAT3 normal;
 	DirectX::XMFLOAT3 tangent;
 };
@@ -17,27 +23,22 @@ struct Vertex {
 class Mesh
 {
 public:
-	Mesh();
 	Mesh(ID3D11Device* pp_device, const char* pp_filepath);
 	~Mesh();
 
 	ID3D11Buffer* GetVertexBuffer();
 	ID3D11Buffer* GetIndexBuffer();
-	std::vector<Vertex> GetVertices();
-	int GetVertexCount();
-	int GetIndexCount();
+	size_t GetVertexCount();
+	size_t GetIndexCount();
 
 private:
-	std::vector<Vertex> vertices;
+	bool LoadMesh(const char* filepath);
+	void CreateBuffers(ID3D11Device * pp_device, Vertex * pp_vertices, size_t p_vertex_count, UINT * pp_indices, size_t p_index_count);
 
-	// Buffers
+	std::vector<Vertex> m_vertices;
+	std::vector<UINT> m_indices;
+	size_t m_vertex_count;
+	size_t m_index_count;
 	Microsoft::WRL::ComPtr<ID3D11Buffer> mcp_vertex_buffer;
 	Microsoft::WRL::ComPtr<ID3D11Buffer> mcp_index_buffer;
-
-	// Counts
-	int m_vertex_count;
-	int m_index_count;
-
-	void CreateBuffers(Vertex* vertices, int numVerts, unsigned int* indices, int numInds, ID3D11Device* device);
-	void CalculateTangents(Vertex* verts, int numVerts, unsigned int* indices, int numIndices);
 };
